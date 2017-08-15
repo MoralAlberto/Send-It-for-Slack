a9ea8bfeecb48fd8c0c9c1e8a817c1806446a84b
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-15 19:38:49 +0200

    Initial Commit

---
+++ Slackfari/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slackfari
+//
+//  Created by Alberto Moral on 15/08/2017.
+//  Copyright © 2017 Alberto Moral. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Slackfari/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slackfari
+//
+//  Created by Alberto Moral on 15/08/2017.
+//  Copyright © 2017 Alberto Moral. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
