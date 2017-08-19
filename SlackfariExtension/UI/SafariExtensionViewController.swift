a9396268cbd3d01a65277e41f17107ae6c8a5a26
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-19 20:19:57 +0200

    Add NSTableView code

--- SlackfariExtension/UI/SafariExtensionViewController.swift
+++ SlackfariExtension/UI/SafariExtensionViewController.swift
@@ -13,41 +13,36 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
     static let shared = SafariExtensionViewController()
 
     @IBOutlet weak var tableView: NSTableView!
-//    @IBOutlet weak var tableView: NSTableView!
     
 //    let presenter = Presenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        
-//        tableView.delegate = nil
-//        tableView.dataSource = self
-//        tableView.rowSizeStyle = .large
-//        tableView.backgroundColor = NSColor.clear
+        
+        tableView.delegate = nil
+        tableView.dataSource = self
+        tableView.rowSizeStyle = .large
+        tableView.backgroundColor = NSColor.clear
     }
 }
 
-//extension SafariExtensionViewController: NSTableViewDataSource {
-//
-//    func numberOfRows(in tableView: NSTableView) -> Int {
-//        return 10
-//    }
-//    
-//    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-//        var cellIdentifier: String = ""
-//        
-//        if tableColumn == tableView.tableColumns[0] {
-//            cellIdentifier = "NameCellID"
-//        }
-//        
-//        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
-//            cell.textField?.stringValue = "Hola"
-//            return cell
-//        }
-//        return nil
-//    }
-//    
-//    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-//        return 80
-//    }
-//}
+extension SafariExtensionViewController: NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellIdentifier = "NameCellID"
+        
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: self) as? NSTableCellView {
+            cell.textField?.stringValue = "Hello"
+            return cell
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return 80
+    }
+}
