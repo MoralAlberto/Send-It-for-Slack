a8955c6f1f1064973d07a483cbdca4f50dd4966c
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-20 21:16:33 +0200

    Add all use cases in presenter

--- SlackfariExtension/UI/Presenter.swift
+++ SlackfariExtension/UI/Presenter.swift
@@ -10,22 +10,56 @@ import Foundation
 import SlackWebAPIKit
 import RxSwift
 
+enum MessageType {
+    case user
+    case group
+    case channel
+}
+
 class Presenter {
     
     fileprivate let getChannelsUseCase: ChannelsListUseCaseProtocol
+    fileprivate let getUsersUseCase: UsersListUseCaseProtocol
+    fileprivate let getGroupsUseCase: GroupsListUseCaseProtocol
+    
     fileprivate let postChannelMessage: FindChannelAndPostMessageUseCaseProtocol
+    fileprivate let postUserMessage: FindUserAndPostMessageUseCaseProtocol
+    fileprivate let postPrivateChannelMessage: FindGroupAndPostMessageUseCaseProtocol
     
     init(getChannelsUseCase: ChannelsListUseCaseProtocol = ChannelsListUseCase(),
-         postChannelMessage: FindChannelAndPostMessageUseCaseProtocol = FindChannelAndPostMessageUseCase()) {
+         getUsersUseCase: UsersListUseCaseProtocol = UsersListUseCase(),
+         getPrivateChannelsUseCase: GroupsListUseCaseProtocol = GroupsListUseCase(),
+         postChannelMessage: FindChannelAndPostMessageUseCaseProtocol = FindChannelAndPostMessageUseCase(),
+         postUserMessage: FindUserAndPostMessageUseCaseProtocol = FindUserAndPostMessageUseCase(),
+         postPrivateChannelMessage: FindGroupAndPostMessageUseCaseProtocol = FindGroupAndPostMessageUseCase()) {
         self.getChannelsUseCase = getChannelsUseCase
+        self.getUsersUseCase = getUsersUseCase
+        self.getGroupsUseCase = getPrivateChannelsUseCase
         self.postChannelMessage = postChannelMessage
+        self.postUserMessage = postUserMessage
+        self.postPrivateChannelMessage = postPrivateChannelMessage
     }
     
     func getChannels() -> Observable<[Channel]> {
         return getChannelsUseCase.execute()
     }
     
-    func send(message: String, channel: String) -> Observable<Bool> {
-        return postChannelMessage.execute(text: message, channel: channel)
+    func getUsers() -> Observable<[User]> {
+        return getUsersUseCase.execute()
+    }
+    
+    func getGroups() -> Observable<[Group]> {
+        return getGroupsUseCase.execute()
+    }
+    
+    func send(message: String, channel: String, type: MessageType) -> Observable<Bool> {
+        switch type {
+        case .user:
+            return postUserMessage.execute(text: message, user: channel)
+        case .channel:
+            return postChannelMessage.execute(text: message, channel: channel)
+        case .group:
+            return postPrivateChannelMessage.execute(text: message, group: channel)
+        }
     }
 }
