800f17ce74113cbe18dc2426f3526b3fe5f37895
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-20 21:15:17 +0200

    Add channel view model

---
+++ SlackfariExtension/UI/ViewModels/ChannelViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChannelViewModel.swift
+//  Slackfari
+//
+//  Created by Alberto Moral on 20/08/2017.
+//  Copyright © 2017 Alberto Moral. All rights reserved.
+//
+
+import Foundation
+
+struct ChannelViewModel: Channelable {
+    var name: String
+}
