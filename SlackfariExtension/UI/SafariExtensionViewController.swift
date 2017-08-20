3cacfe8c06059e436fa1f45d67c7c2af2ebc0437
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-20 21:16:46 +0200

    Add channeable protocol

--- SlackfariExtension/UI/SafariExtensionViewController.swift
+++ SlackfariExtension/UI/SafariExtensionViewController.swift
@@ -10,51 +10,86 @@ import SafariServices
 import SlackWebAPIKit
 import RxSwift
 
-class SafariExtensionViewController: SFSafariExtensionViewController, NSTableViewDataSource, NSTableViewDelegate {
+protocol Channelable {
+    var name: String { get }
+}
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
     
     static let shared = SafariExtensionViewController()
 
     @IBOutlet weak var tableView: NSTableView!
     
-    var presenter: Presenter?
-    let disposeBag = DisposeBag()
+    fileprivate var presenter: Presenter?
+    fileprivate let disposeBag = DisposeBag()
     
-    var items = [Channel]()
-    
+    fileprivate var items = [Channelable]()
     var url: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         API.sharedInstance.set(token: "")
-        
+        configureTableView()
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        presenter = Presenter()
+        getAllChannels()
+    }
+    
+    private func configureTableView() {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowSizeStyle = .large
         tableView.backgroundColor = NSColor.clear
-        
-        presenter = Presenter()
-        presenter?.getChannels()
-            .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { [weak self] channels in
-            guard let strongSelf = self else { return }
-            strongSelf.items = channels
-            strongSelf.tableView.reloadData()
-            print("Channels \(channels)")
-        }, onError: { error in
-            print("Error \(error)")
-        }).addDisposableTo(disposeBag)
     }
     
     @IBAction func sendMessage(_ sender: Any) {
         let selected = items[tableView.selectedRow]
-        presenter?.send(message: url ?? "", channel: selected.name ?? "").subscribe(onNext: { isSent in
+        guard let post = url else { return }
+        let type = checkChannel(type: selected)
+        send(message: post, toChannel: selected.name, withType: type)
+    }
+    
+    private func getAllChannels() {
+        Observable.combineLatest(presenter!.getUsers(), presenter!.getChannels(), presenter!.getGroups())
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] (users, channels, groups) in
+                guard let strongSelf = self else { return }
+                let usersViewModel: [Channelable] = users.map(UserViewModel.init)
+                let channelsViewModel: [Channelable] = channels.map(ChannelViewModel.init)
+                let groupsViewModel: [Channelable] = groups.map(GroupViewModel.init)
+                
+                strongSelf.items = usersViewModel + channelsViewModel + groupsViewModel
+                strongSelf.tableView.reloadData()
+                }, onError: { error in
+                    print("Error \(error)")
+            }
+        ).disposed(by: disposeBag)
+    }
+    
+    private func checkChannel(type: Channelable) -> MessageType {
+        if type is ChannelViewModel {
+            return .channel
+        } else if type is GroupViewModel {
+            return .group
+        } else {
+            return .user
+        }
+    }
+    
+    private func send(message: String, toChannel channel: String, withType type: MessageType) {
+        presenter?.send(message: message, channel: channel, type: type).subscribe(onNext: { isSent in
             print("message sent")
         }, onError: { (error) in
             print("Error \(error)")
         }).disposed(by: disposeBag)
     }
-    
+}
+
+extension SafariExtensionViewController: NSTableViewDataSource, NSTableViewDelegate {
     func numberOfRows(in tableView: NSTableView) -> Int {
         return items.count
     }
@@ -65,7 +100,7 @@ class SafariExtensionViewController: SFSafariExtensionViewController, NSTableVie
         if let cell = tableView.make(withIdentifier: cellIdentifier, owner: self) as? NSTableCellView {
             let item = items[row]
             let name = item.name
-            cell.textField?.stringValue = name ?? "No name"
+            cell.textField?.stringValue = name
             return cell
         }
         return nil
