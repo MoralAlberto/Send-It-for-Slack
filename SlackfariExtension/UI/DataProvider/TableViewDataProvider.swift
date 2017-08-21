a34ac879171cad54cfcae201da14ac4faf83040f
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-21 20:42:14 +0200

    Create table view data provider to handle all channels logic

--- SlackfariExtension/UI/DataProvider/TableViewDataProvider.swift
+++ SlackfariExtension/UI/DataProvider/TableViewDataProvider.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import Cocoa
+
+class TableViewDataProvider: NSObject {
+    
+    fileprivate var items = [Channelable]()
+    private var tableView: NSTableView
+    
+    init(tableView: NSTableView) {
+        self.tableView = tableView
+        super.init()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func getItem(at index: Int) -> Channelable? {
+        guard index <= items.count else { return nil }
+        return items[index]
+    }
+    
+    func set(items: [Channelable]) {
+        self.items = items
+    }
+}
+
+extension TableViewDataProvider: NSTableViewDataSource, NSTableViewDelegate {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellIdentifier = "NameCellID"
+        
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: self) as? NSTableCellView {
+            let item = items[row]
+            let name = item.name
+            cell.textField?.stringValue = name
+            return cell
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return 26
+    }
+}
