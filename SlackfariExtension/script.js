1e81e1cc4ddcf9802c64029c270f8e78f57d082b
Alberto Moral <alberto.moral.g@gmail.com>
2017-09-19 17:28:12 +0200

    Delete console.log in script.js

--- SlackfariExtension/script.js
+++ SlackfariExtension/script.js
@@ -2,6 +2,5 @@ safari.self.addEventListener("message", handleMessage);
 
 function handleMessage(message) {
     var selectedText = window.getSelection().toString();
-    console.log("selected " + selectedText)
     safari.extension.dispatchMessage("getURL", { "selectedText": selectedText });
 }
