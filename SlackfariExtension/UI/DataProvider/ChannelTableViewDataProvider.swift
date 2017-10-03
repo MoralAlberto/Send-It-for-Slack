d2a4551cd314949a82b5e4971bac02719ee6286e
Alberto Moral <alberto.moral.g@gmail.com>
2017-10-03 19:44:49 +0200

    Add license, readme and gifs

--- SlackfariExtension/UI/DataProvider/ChannelTableViewDataProvider.swift
+++ SlackfariExtension/UI/DataProvider/ChannelTableViewDataProvider.swift
@@ -1,10 +1,9 @@
-//
-//  TableViewDataProvider.swift
-//  Slackfari
-//
-//  Created by Alberto Moral on 21/08/2017.
-//  Copyright © 2017 Alberto Moral. All rights reserved.
-//
+/**
+ *  Slackfari
+ *  Copyright (c) 2017 Alberto Moral
+ *  Licensed under the MIT license, see LICENSE file
+ */
+
 
 import Cocoa
 
